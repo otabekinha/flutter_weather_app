fdc895e2a549007f2257be58efa1e8656b7445d8
otabekinha <otabekinha@gmail.com>
2024-01-18 12:03:45 +0900

    Done with initial layout of the weather app

---
+++ lib/weather_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class WeatherScreen extends StatelessWidget {
+  const WeatherScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Weather App",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.refresh,
+            ),
+          ),
+        ],
+      ),
+      body: const Column(
+        children: [
+          // main card
+          Placeholder(
+            fallbackHeight: 250,
+          ),
+          SizedBox(height: 20),
+          // weather forecast cards
+          Placeholder(
+            fallbackHeight: 150,
+          ),
+          SizedBox(height: 20),
+          // additional information
+          Placeholder(
+            fallbackHeight: 150,
+          ),
+        ],
+      ),
+    );
+  }
+}
